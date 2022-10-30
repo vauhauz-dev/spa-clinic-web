fc15967360194f19ba2736493154e459c0fded24
jaime-alex <jaime-alex@outlook.com>
2022-10-29 20:33:47 -0700

    Update login.tsx

--- src/components/login.tsx
+++ src/components/login.tsx
@@ -2,9 +2,25 @@
 export default function Login () {
     return (
         <>
+        <h1>Iskali Natural Spa</h>
             <div className="container" id="container">
                 <div className="form-container sign-up-container">
-                <form action="#">
+                    <form action="#">
+                        <h1>Create Account</h1>
+                        <div className="social-container">
+                            <a href="#" className="social"><i className="fab fa-facebook-f"></i></a>
+                            <a href="#" className="social"><i className="fab fa-google-plus-g"></i></a>
+                            <a href="#" className="social"><i className="fab fa-linkedin-in"></i></a>
+                        </div>
+                        <span>or use your email for registration</span>
+                        <input type="text" placeholder="Name" />
+                        <input type="email" placeholder="Email" />
+                        <input type="password" placeholder="Password" />
+                        <button>Sign Up</button>
+                    </form>
+                </div>
+                <div className="form-container sign-in-container">
+                    <form action="#">
                         <h1 className="signin-title">Sign in</h1>
                         <div className="social-container">
                             <a href="#" className="social"><i className="fab fa-facebook-f"></i></a>
@@ -18,9 +34,6 @@ export default function Login () {
                         <button>Sign In</button>
                         <button>Sign Up</button>
                     </form>
-                </div>
-                <div className="form-container sign-in-container">
-                    
                 </div>
                 <div className="overlay-container">
                     <div className="overlay">

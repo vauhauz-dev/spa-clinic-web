d9a93eba6df6547854805b358662026a35d6cbaf
jaime-alex <jaime-alex@outlook.com>
2022-10-29 17:37:50 -0700

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,12 +13,11 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!!!</a>
+          Iskali
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.tsx</code>
+          Natural SPA
         </p>
 
         <div className={styles.grid}>

2165f580b15caff6441469a5a98496187e244af8
jaime-alex <jaime-alex@outlook.com>
2022-10-29 20:27:36 -0700

    updates

--- src/components/login.tsx
+++ src/components/login.tsx
@@ -2,25 +2,10 @@
 export default function Login () {
     return (
         <>
-        <h1>Iskali Natural Spa</h1>
+        <h1>Iskali Natural Spa</h>
             <div className="container" id="container">
                 <div className="form-container sign-up-container">
-                    <form action="#">
-                        <h1>Create Account</h1>
-                        <div className="social-container">
-                            <a href="#" className="social"><i className="fab fa-facebook-f"></i></a>
-                            <a href="#" className="social"><i className="fab fa-google-plus-g"></i></a>
-                            <a href="#" className="social"><i className="fab fa-linkedin-in"></i></a>
-                        </div>
-                        <span>or use your email for registration</span>
-                        <input type="text" placeholder="Name" />
-                        <input type="email" placeholder="Email" />
-                        <input type="password" placeholder="Password" />
-                        <button>Sign Up</button>
-                    </form>
-                </div>
-                <div className="form-container sign-in-container">
-                    <form action="#">
+                <form action="#">
                         <h1 className="signin-title">Sign in</h1>
                         <div className="social-container">
                             <a href="#" className="social"><i className="fab fa-facebook-f"></i></a>
@@ -32,7 +17,11 @@ export default function Login () {
                         <input type="password" placeholder="Password" />
                         <a href="#">Forgot your password?</a>
                         <button>Sign In</button>
+                        <button>Sign Up</button>
                     </form>
+                </div>
+                <div className="form-container sign-in-container">
+                    
                 </div>
                 <div className="overlay-container">
                     <div className="overlay">

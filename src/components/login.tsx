681ef1b8ae66e52347e21f43478be97ca1210692
jaime-alex <jaime-alex@outlook.com>
2022-10-29 20:38:22 -0700

    Update login.tsx

--- src/components/login.tsx
+++ src/components/login.tsx
@@ -4,36 +4,8 @@ export default function Login () {
         <>
         <h1>Iskali Natural Spa</h1>
             <div className="container" id="container">
-                <div className="form-container sign-up-container">
-                    <form action="#">
-                        <h1>Create Account</h1>
-                        <div className="social-container">
-                            <a href="#" className="social"><i className="fab fa-facebook-f"></i></a>
-                            <a href="#" className="social"><i className="fab fa-google-plus-g"></i></a>
-                            <a href="#" className="social"><i className="fab fa-linkedin-in"></i></a>
-                        </div>
-                        <span>or use your email for registration</span>
-                        <input type="text" placeholder="Name" />
-                        <input type="email" placeholder="Email" />
-                        <input type="password" placeholder="Password" />
-                        <button>Sign Up</button>
-                    </form>
-                </div>
                 <div className="form-container sign-in-container">
-                    <form action="#">
-                        <h1 className="signin-title">Sign in</h1>
-                        <div className="social-container">
-                            <a href="#" className="social"><i className="fab fa-facebook-f"></i></a>
-                            <a href="#" className="social"><i className="fab fa-google-plus-g"></i></a>
-                            <a href="#" className="social"><i className="fab fa-linkedin-in"></i></a>
-                        </div>
-                        <span>or use your account</span>
-                        <input type="email" placeholder="Email" />
-                        <input type="password" placeholder="Password" />
-                        <a href="#">Forgot your password?</a>
-                        <button>Sign In</button>
-                        <button>Sign Up</button>
-                    </form>
+                    
                 </div>
                 <div className="overlay-container">
                     <div className="overlay">
@@ -43,8 +15,20 @@ export default function Login () {
                             <button className="ghost" id="signIn">Sign In</button>
                         </div>
                         <div className="overlay-panel overlay-right">
-                            <h1>Hello, Friend!</h1>
-                            <p>Enter your personal details and start journey with us</p>
+                            <form action="#">
+                                <h1 className="signin-title">Sign in</h1>
+                                <div className="social-container">
+                                    <a href="#" className="social"><i className="fab fa-facebook-f"></i></a>
+                                    <a href="#" className="social"><i className="fab fa-google-plus-g"></i></a>
+                                    <a href="#" className="social"><i className="fab fa-linkedin-in"></i></a>
+                                </div>
+                                <span>or use your account</span>
+                                <input type="email" placeholder="Email" />
+                                <input type="password" placeholder="Password" />
+                                <a href="#">Forgot your password?</a>
+                                <button>Sign In</button>
+                                <button>Sign Up</button>
+                            </form>
                             <button className="ghost" id="signUp">Sign Up</button>
                         </div>
                     </div>

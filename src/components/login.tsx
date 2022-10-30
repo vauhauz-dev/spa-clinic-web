1eb1457ea7aa417c02e6cb3e4e5e5b072faee4bb
jaime-alex <jaime-alex@outlook.com>
2022-10-29 20:34:28 -0700

    Update login.tsx

--- src/components/login.tsx
+++ src/components/login.tsx
@@ -2,7 +2,7 @@
 export default function Login () {
     return (
         <>
-        <h1>Iskali Natural Spa</h>
+        <h1>Iskali Natural Spa</h1>
             <div className="container" id="container">
                 <div className="form-container sign-up-container">
                     <form action="#">

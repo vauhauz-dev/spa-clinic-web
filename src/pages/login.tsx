944609611768c5ac0b71fbb4f185be55dd41d387
jaime-alex <jaime-alex@outlook.com>
2022-10-30 18:59:18 -0700

    Update login.tsx

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -28,8 +28,6 @@ export default function Login() {
   };
 
   return (
-    <ThemeProvider theme={theme}>
-      <Container id="login-container">
         <Grid2 container spacing={2}>
           <Grid2 xs={8}>
 
@@ -99,7 +97,5 @@ export default function Login() {
             <Copyright sx={{ mt: 8, mb: 4 }} />
           </Grid2>
         </Grid2>
-      </Container>
-    </ThemeProvider>
   );
 }

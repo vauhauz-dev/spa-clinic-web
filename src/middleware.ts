d6a283aa9c27796b0b2e1bc44d58f3a812dcfffd
jaime-alex <jaime-alex@outlook.com>
2022-10-30 20:41:43 -0700

    Create middleware.ts

---
+++ src/middleware.ts
@@ -0,0 +1,25 @@
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+
+// This function can be marked `async` if using `await` inside
+export function middleware(request: NextRequest) {
+    const cookie = request.cookies.get('bearer')
+
+    if (!cookie) {
+        console.log('No bearer cookie found.')
+        return NextResponse.redirect(new URL('/login', request.url))
+    }
+}
+
+export const config = {
+    matcher: [
+      /*
+       * Match all request paths except for the ones starting with:
+       * - api (API routes)
+       * - static (static files)
+       * - favicon.ico (favicon file)
+       */
+      '/((?!api|static|favicon.ico|vercel.svg|login).*)'
+    ],
+  }
+  

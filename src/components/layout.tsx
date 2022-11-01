00016c4f931d14d358905e2f97bf34ea3304a773
jaime-alex <jaime-alex@outlook.com>
2022-10-31 22:15:43 -0700

    Update layout.tsx

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -15,15 +15,14 @@ export default function Layout({ children }: { children: ReactNode }) {
             if (!authState && !router.route.includes('login')) {
                 requireRedirect = true;
                 router.push('/login')
-                setShowContent(true);
             }
 
             if (authState && router.route.includes('login')) {
                 requireRedirect = true;
                 router.push('/')
-                setShowContent(true);
             }
 
+            setShowContent(true);
         })
         return () => unsubscribe();
     },[])

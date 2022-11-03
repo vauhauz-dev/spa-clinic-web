2012201c197b57bbaedb33615a306924311c86a8
jaime-alex <jaime-alex@outlook.com>
2022-11-03 16:13:07 -0700

    Update nav-bar.tsx

--- src/components/nav-bar.tsx
+++ src/components/nav-bar.tsx
@@ -10,7 +10,7 @@ export default function NavBar() {
                 <IconButton sx={{ p: 0 }}>
                     <Avatar alt="Remy Sharp" src={logoImageUrl} />
                 </IconButton>
-                <Typography variant="h6" color="inherit" component="div">
+                <Typography variant="h6" color="inherit" component="div" marginLeft="15px">
                     Photos
                 </Typography>
             </Toolbar>

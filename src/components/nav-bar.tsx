8c088052e9842a68100e4ebdf47f2205782a91a9
jaime-alex <jaime-alex@outlook.com>
2022-11-03 16:01:55 -0700

    Update nav-bar.tsx

--- src/components/nav-bar.tsx
+++ src/components/nav-bar.tsx
@@ -1,8 +1,18 @@
-import { AppBar } from "@mui/material";
+import { AppBar, IconButton, Toolbar, Typography } from "@mui/material";
+import MenuIcon from '@mui/icons-material/Menu';
 
 export default function NavBar() {
     return <div>
-        <AppBar position="static"></AppBar>
+        <AppBar position="static">
+            <Toolbar variant="dense">
+                <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
+                    <MenuIcon />
+                </IconButton>
+                <Typography variant="h6" color="inherit" component="div">
+                    Photos
+                </Typography>
+            </Toolbar>
+        </AppBar>
         Nav Bar!!
     </div>
 }

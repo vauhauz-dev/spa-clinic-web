bddafb092b847da1e7b37b0a933386a1c3a78ea9
jaime-alex <jaime-alex@outlook.com>
2022-11-07 21:12:24 -0800

    Update customers.tsx

--- src/components/clientes/customers.tsx
+++ src/components/clientes/customers.tsx
@@ -1,24 +1,23 @@
-import { alpha, Box, InputBase, Paper, styled, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Toolbar, Typography } from "@mui/material";
+import { alpha, Box, Chip, InputBase, Paper, styled, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Toolbar, Typography } from "@mui/material";
 import { Container } from "@mui/system";
 import SearchIcon from '@mui/icons-material/Search';
 import Grid from '@mui/material/Unstable_Grid2';
 
 function createData(
     name: string,
-    calories: number,
-    fat: number,
-    carbs: number,
-    protein: number,
+    calories: string,
+    fat: string,
+    carbs: string,
   ) {
-    return { name, calories, fat, carbs, protein };
+    return { name, calories, fat, carbs };
   }
   
   const rows = [
-    createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-    createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-    createData('Eclair', 262, 16.0, 24, 6.0),
-    createData('Cupcake', 305, 3.7, 67, 4.3),
-    createData('Gingerbread', 356, 16.0, 49, 3.9),
+    createData('Frozen yoghurt', 'Culiacan', 'COMPLETO', '01/01/2022'),
+    createData('Ice cream sandwich', 'Tijuana', 'COMPLETO', '01/01/2022'),
+    createData('Eclair', 'Culiacan', 'PENDIENTE', '01/01/2022'),
+    createData('Cupcake', 'Tijuana', 'COMPLETO', '01/01/2022'),
+    createData('Gingerbread', 'Culiacan', 'PENDIENTE', '01/01/2022'),
   ];
 
   const SearchIconWrapper = styled('div')(({ theme }) => ({
@@ -84,14 +83,13 @@ export default function Customers() {
             </Grid>
         </Paper>
         <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+      <Table sx={{ minWidth: 500 }} aria-label="simple table">
         <TableHead>
           <TableRow>
-            <TableCell>Dessert (100g serving)</TableCell>
-            <TableCell align="right">Calories</TableCell>
-            <TableCell align="right">Fat&nbsp;(g)</TableCell>
-            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-            <TableCell align="right">Protein&nbsp;(g)</TableCell>
+            <TableCell>Nombre</TableCell>
+            <TableCell align="center">Ciudad</TableCell>
+            <TableCell align="center">Estatus</TableCell>
+            <TableCell align="center">Ultimo pago</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
@@ -103,10 +101,11 @@ export default function Customers() {
               <TableCell component="th" scope="row">
                 {row.name}
               </TableCell>
-              <TableCell align="right">{row.calories}</TableCell>
-              <TableCell align="right">{row.fat}</TableCell>
-              <TableCell align="right">{row.carbs}</TableCell>
-              <TableCell align="right">{row.protein}</TableCell>
+              <TableCell align="center">{row.calories}</TableCell>
+              <TableCell align="center" >
+                <Chip onClick={() =>{}} label={row.fat} sx={{color: row.fat == 'COMPLETO' ? 'green' : 'orange'}} variant="outlined" />
+              </TableCell>
+              <TableCell align="center">{row.carbs}</TableCell>
             </TableRow>
           ))}
         </TableBody>

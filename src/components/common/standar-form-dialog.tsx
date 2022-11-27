6dc5510cd87b41d6a414ab237f7c9441bee8397d
jaime-alex <jaime-alex@outlook.com>
2022-11-27 08:42:39 -0800

    Update standar-form-dialog.tsx

--- src/components/common/standar-form-dialog.tsx
+++ src/components/common/standar-form-dialog.tsx
@@ -1,8 +1,10 @@
-import { Dialog, DialogTitle, DialogContent, DialogContentText, TextField, DialogActions, Button, Card, Skeleton, CardHeader } from "@mui/material";
+import { Dialog, DialogTitle, DialogContent, DialogContentText, TextField, DialogActions, Button, Card, Skeleton, CardHeader, CardContent } from "@mui/material";
+import { Fragment } from "react";
 
 export default function StandarFormDialog(props: any) {
     return <>
-        <Dialog open={props.open} onClose={props.handleClose}>
+        <Dialog fullWidth={true}
+        maxWidth={'sm'} open={props.open} onClose={props.handleClose}>
         <DialogTitle>Subscribe</DialogTitle>
         <DialogContent>
           <Card>
@@ -17,6 +19,13 @@ export default function StandarFormDialog(props: any) {
         }
         subheader={<Skeleton animation="wave" height={10} width="40%" />}
       />
+      <Skeleton sx={{ height: 190 }} animation="wave" variant="rectangular" />
+      <CardContent>
+      <Fragment>
+            <Skeleton animation="wave" height={10} style={{ marginBottom: 6 }} />
+            <Skeleton animation="wave" height={10} width="80%" />
+          </Fragment>
+      </CardContent>
           </Card>
         </DialogContent>
         <DialogActions>
